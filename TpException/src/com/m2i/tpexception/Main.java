e7b634bf32e7a7c273c8f5a082f9796c7a8a3c9f
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-15 09:58:46 +0200

    tp exception

--- TpException/src/com/m2i/tpexception/Main.java
+++ TpException/src/com/m2i/tpexception/Main.java
@@ -3,7 +3,21 @@ package com.m2i.tpexception;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		try{	
+			int a =2;
+			int b =0;
+			int c = a/b;
+			System.out.println(c);
+		}
+		catch(ArithmeticException e) {
+		
+			System.err.println("Erreur !");
+			System.err.println(e);
+		}
+		
+		
+			
 
 	}
 
