3eda73ac0d5ebb3c4d0c2bc6810d051af6c28fba
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-15 14:48:52 +0200

    tp exception

--- TPIO/src/com/m2i/tpio/Main.java
+++ TPIO/src/com/m2i/tpio/Main.java
@@ -1,12 +1,33 @@
 package com.m2i.tpio;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		System.out.println("Votre nom : ");
+		// BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		// String nom = in.readLine();
+		String nom = System.console().readLine();
+		System.out.println("Bonjour " + nom);
+	}
+
+	public static void main_read(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new FileReader("le-fichier.txt"));
+
+		) {
+
+			br.lines().forEach(System.out::println);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public static void main_write(String[] args) {
