52da57232c927f4b7020ead4f4754fca54688394
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-16 13:44:41 +0200

    tp json et http

--- tpjson/src/main/java/com/m2i/tpjson/Main.java
+++ tpjson/src/main/java/com/m2i/tpjson/Main.java
@@ -1,20 +1,60 @@
 package com.m2i.tpjson;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
 public class Main {
 
 	public static void main(String[] args) {
+		final String fileName = "list_rectangle.json";
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			List<Rectangle> todos = objectMapper.readValue(new File(fileName), new TypeReference<List<Rectangle>>() {
+			});
+
+			System.out.println(todos);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main_gson_read_collection(String[] args) {
+
+		try {
+			final String fileName = "list_rectangle.json";
+			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+			Type type = new TypeToken<ArrayList<Rectangle>>() {
+			}.getType();
+
+			List<Rectangle> l = gson.fromJson(new FileReader(fileName), type);
+
+			l.forEach(System.out::println);
+
+		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main_gson_write_collection(String[] args) {
 		Rectangle r1 = new Rectangle(1, 2);
 		Rectangle r2 = new Rectangle(11, 12);
 		Rectangle r3 = new Rectangle(21, 22);
---
+++ tpjson/src/main/java/com/m2i/tpjson/MainHttp.java
@@ -0,0 +1,10 @@
+package com.m2i.tpjson;
+
+public class MainHttp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
