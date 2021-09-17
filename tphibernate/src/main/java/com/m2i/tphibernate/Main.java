6bec7accff541b91ad8ee905690a902fb20360b7
Frédéric Gaurat <fgaurat@eolem.com>
2021-09-17 10:30:25 +0200

    JDBC et DAO

--- tphibernate/src/main/java/com/m2i/tphibernate/Main.java
+++ tphibernate/src/main/java/com/m2i/tphibernate/Main.java
@@ -1,10 +1,18 @@
 package com.m2i.tphibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		Session session = sessionFactory.openSession();
 	}
 
 }
